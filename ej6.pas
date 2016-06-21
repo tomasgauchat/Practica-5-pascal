854d85b3af8fd0f9b35ea97649c236b678d55c48
brian070597 <brianschueris@hotmail.com>
2016-06-21 19:49:12 -0300

    ejercicio seis

--- ej6.pas
+++ ej6.pas
@@ -3,7 +3,7 @@ program ej6;
 uses crt;
 
 var vueltas: integer;
-    contador: integer;
+    //contador: integer;
     a: integer;
     b: integer;
     c: integer;
@@ -11,59 +11,58 @@ var vueltas: integer;
     
 
 begin
- readln(vueltas);
+        readln(vueltas);
 
         readln(a);
         readln(b);
-        readln(c);
-        contador:= 3;
- for i:= 1 to vueltas-2 do
-       begin
-       contador:= contador + 1;
-       
-       if (a>b) and (a>c) then
-               begin
-               write(a);
-               writeln(' es el mayor');
-               write('su posicion es ');
-               writeln('1/3');
-               end
-       else
-               begin
-               if (b>a) and (b>c) then
-                 begin
-                      write(b);
-                      writeln(' es el mayor');
-                      write('su posicion es ');
-                      writeln('2/3');
-                  end
-               else
-               if (c>a) and (c>b) then
-                    begin
-                    write(c);
-                      writeln(' es el mayor');
-                      write('su posicion es ');
-                      writeln('3/3');
-                    end
+        
+        //contador:= 2;
+
+        for i:= 3 to vueltas do
+        begin
+                readln(c);
+
+                //contador:= contador + 1;
+                
+                if (a>b) and (a>c) then
+                begin
+                        write(a);
+                        writeln(' es el mayor');
+                        write('su posicion es ');
+                        writeln('1/3');
+                end
                 else
-                    begin
-                    writeln('hay 2 o mas numeros iguales');
-                    end;
+                begin
+                        if (b>a) and (b>c) then
+                        begin
+                            write(b);
+                            writeln(' es el mayor');
+                            write('su posicion es ');
+                            writeln('2/3');
+                        end
+                        else
+                        begin
+                            if (c>a) and (c>b) then
+                            begin
+                                write(c);
+                                writeln(' es el mayor');
+                                write('su posicion es ');
+                                writeln('3/3');
+                            end
+                            else
+                            begin
+                                writeln('hay 2 o mas numeros iguales');
+                            end;
+                        end;
+
+                        //if contador <= vueltas then
+                        //begin
+                        //    readln(c);        
+                        //end;
                 end;
-        a:=b;
-        b:=c;
-        if contador <= vueltas then
-          begin
-          readln(c);        
-          end;
-       
-       
-       
-       
-       end;
+                a:=b;
+                b:=c;
+        end;   //for i:= 1 to vueltas-2 do 
+        readkey();
  
- readkey();
- 
- 
-  
 end.
