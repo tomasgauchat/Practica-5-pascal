da0e292537eac02c1b3291399c30f52610e90d77
tomas gauchat <tomas gauchat>
2016-06-22 12:37:01 -0300

    Corrección final del ej 12

--- Practica5ej12.pas
+++ Practica5ej12.pas
@@ -9,89 +9,58 @@ begin
 readln(numero);
 
 if numero <> 0 then
+
 begin
+
+      ultimo := numero;  
       readln(numero);
+      
       if numero<> 0 then
         begin
-          //codigo programa
-        end;  
-end;
-
-
-
-ultimo := 0;
-anteultimo := 0;
-
-readln(numero);
-
-if numero <> 0 then
-begin
-            ultimo := numero;
-end;
-
-if ultimo <> 0 then
-begin
-            readln(numero);
-end;
-
-
-if (ultimo > numero) and (numero <> 0) then
-begin
-            anteultimo := numero;
-end
-else
-begin
-            anteultimo := ultimo;
-            ultimo := numero 
-end;
-
-if (numero <> 0) and (ultimo <> 0) then
-begin
-            readln(numero);
-end;
-
-while (numero <> 0) and (ultimo <> 0) do
-begin
-        if numero > ultimo then
-        begin
-                anteultimo := ultimo;
-                ultimo := numero;
-        end
-        else
-        begin
-                if numero > anteultimo then
+         while (numero <> 0) and (ultimo <> 0) do
                 begin
-                    anteultimo := numero;
-                end;
-        end;
-        readln(numero);
-end;
+                        if numero > ultimo then
+                                begin
+                                        anteultimo := ultimo;
+                                        ultimo := numero;
+                                end
+                                else
+                                begin
+                                        if numero > anteultimo then
+                                                begin
+                                                        anteultimo := numero;
+                                                end;//if numero > anteultimo then
+                                end; //if numero<> 0 then
+                        readln(numero);
+                end; //while (numero <> 0) and (ultimo <> 0) do
+        end; //if numero<> 0 then
+end; //if numero <> 0 then
+
 if ultimo <> 0 then
 begin
         write('El numero mas grande es ');
         writeln(ultimo);
         if anteultimo <> 0 then
-        begin
-                Write('El segundo numero mas grande es ');
-                writeln(anteultimo);
-        end
-        else
-        begin
-                writeln(ultimo);
-                writeln('No hay un segundo numero');  
-        end;
+                begin
+                        Write('El segundo numero mas grande es ');
+                        writeln(anteultimo);
+                end
+                else
+                begin
+                        writeln('No hay un segundo numero');  
+                end; // if anteultimo <> 0 then
 end
 else
 begin
         if anteultimo <> 0 then
-        begin
-                write('El numero mas grande es ');
-                writeln(anteultimo);
-        end
-        else
-        begin
-                WriteLn('No hay numeros en su conjunto');
-        end;
-end;
-        
+                begin
+                        write('El numero mas grande es ');
+                        writeln(anteultimo);
+                end
+                else
+                begin
+                        WriteLn('No hay numeros en su conjunto');
+                end; //if anteultimo <> 0 then
+end; //if ultimo <> 0 then
+
 end.
